8863924a1a98b984c3dad9a3032704b4311c99c2
VedantGadge <vedant.gadgegsis@gmail.com>
2025-03-12 12:47:12 +0530

    refactor: improve ShoeCard functionality and update Hero section to manage selected shoe image

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,55 +1,48 @@
-import Buttons from "../components/Buttons"
-import { arrowRight }  from '../assets/icons'
-import { shoes, statistics } from '../constants'
-import { bigShoe1 } from "../assets/images"
-import ShoeCard from "../components/ShoeCard"
+import Buttons from "../components/Buttons";
+import { arrowRight } from '../assets/icons';
+import { shoes, statistics } from '../constants';
+import { bigShoe1 } from "../assets/images";
+import ShoeCard from "../components/ShoeCard";
+
+import { useState } from "react";
+
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
   return (
-    <section id="home" className="w-full border-2 border-red-400 p-2 flex flex-col xl:flex-row justify-center min-h-screen gap-10 max-container ">
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-      
-      <p>
-        Our Summer Collection
-      </p>
-      
-      <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-        <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
-        <br/>
-        <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
-      </h1>
-      
-      <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish nike arrivals, quality comfort and innovation for your active life.</p>
-      
-      <Buttons label="Shop now" iconURL={arrowRight} />
-
-      <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
-          {statistics.map((statistic)=> (<div key={statistic.label}>
-            <p className=" font-palanquin font-bold text-4xl ">{statistic.value}</p>
-            <p className=" font-montserrat leading-7 text-slate-gray ">{statistic.label}</p>
-          </div>))}
-      </div>
-      </div>
-
-      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center ">
-        <img src={bigShoe1} alt="Shoe collection" height={500} width={610} className=" object-contain relative z-10 "/>
-
-        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
-            {shoes.map((shoe)=>(<div key={shoe}>
-              <ShoeCard
-                imgURL={shoe}
-                changeBigShoeImage = {()=>{}}
-                bigShoeImg=""
-              />
-            </div>))}
+    <section id="home" className="w-full min-h-screen flex flex-col xl:flex-row justify-center gap-10 max-container">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start  w-full max-xl:padding-x pt-28">
+        <p className='text-xl font-montserrat text-coral-red'>Our Summer Collections</p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover stylish nike arrivals, quality comfort and innovation for your active life.
+        </p>
+        <Buttons label="Shop now" iconURL={arrowRight} />
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((statistic) => (
+            <div key={statistic.label}>
+              <p className="font-palanquin font-bold text-4xl">{statistic.value}</p>
+              <p className="font-montserrat leading-7 text-slate-gray">{statistic.label}</p>
+            </div>
+          ))}
         </div>
-
+      </div>
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img src={bigShoeImg} alt="Shoe collection" height={500} width={610} className="object-contain relative z-10" />
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard imgURL={shoe} changeBigShoeImage={(shoe) => setBigShoeImg(shoe)} bigShoeImg={bigShoeImg} />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
-
-
- {/* span tags are used to apply specific styling to those parts of the text , independant of others. */}
+export default Hero;
