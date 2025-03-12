aa1fdc3661923f4555d021c05c31344315466036
VedantGadge <vedant.gadgegsis@gmail.com>
2025-03-12 13:36:01 +0530

    feat: add PopularProducts section with product cards and enhance SuperQuality section layout

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,6 +1,27 @@
+import Buttons from "../components/Buttons"
+import { shoe8 } from "../assets/images"
+
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section id="about-us" className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
+      <div className="flex flex-1 flex-col">
+        <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We provide you
+          <br />
+          <span className="text-coral-red inline-block mt-3">Super quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction.</p>
+
+        <div className="mt-11">
+        <Buttons label="View Details" />
+        </div>      
+      </div>
+
+      <div className="flex flex-1 justify-center items-center"><img src={shoe8} alt="shoe8" width={570} height={522} className="object-contain" /></div>
+    </section>
   )
 }
 
