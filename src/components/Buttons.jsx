f0832acc2479d80071ea0fcbf4b04f74008fc790
VedantGadge <vedant.gadgegsis@gmail.com>
2025-03-20 00:48:41 +0530

    feat: enhance Buttons component with customizable styles and add ServiceCard component for Services section

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
-const Buttons = ({label,iconURL}) => {
+const Buttons = ({label, iconURL, backgroundColor, textColor, borderColor}) => {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
-        {label}
-
-        {iconURL && <img src={iconURL} alt="arrow right icon" className='ml-2 rounded-full w-5 h-5 '/>} 
-        {/* only renders button img when there is an iconURL */}
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none ${
+      backgroundColor ? 
+        `${backgroundColor} ${textColor} ${borderColor ? borderColor : ''}`
+        : 
+        'bg-coral-red text-white border-coral-red'
+    } rounded-full`}>
+      {label}
+      {iconURL && <img src={iconURL} alt="arrow right icon" className='ml-2 rounded-full w-5 h-5'/>}
+      {/* only renders button img when there is an iconURL */}
     </button>
   )
 }
